d0d03af6e794ef989f5805ec4ed81737914e75ee
esemlak <erich@semlak.at>
2020-10-21 21:38:31 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,32 @@
-let X2 = 0
+let X3 = 0
 let X1 = 0
+let B = 0
+let X2 = 0
 basic.clearScreen()
-for (let X = 0; X <= 4; X++) {
-    X1 = X
-    X2 = 4 - X
-    for (let A = 0; A <= X2 - X1; A++) {
-        led.plot(X1 + A, X)
-        basic.pause(100)
-    }
-    for (let A = 0; A <= X2 - X1; A++) {
-        led.plot(X2, X1 + A)
-        basic.pause(100)
-    }
-    for (let A = 0; A <= X2 - X1; A++) {
-        led.plot(X2 - A, 4 - X)
-        basic.pause(100)
-    }
-    for (let A = 0; A <= X2 - X1; A++) {
-        led.plot(X1, X2 - A)
-        basic.pause(100)
+while (true) {
+    for (let X = 0; X <= 4; X++) {
+        X2 = 4 - X
+        B = X2 - X
+        for (let A = 0; A <= B; A++) {
+            led.toggle(X + A, X)
+            basic.pause(100)
+        }
+        B = B - 1
+        X1 = X + 1
+        X3 = X2 - 1
+        for (let A = 0; A <= B; A++) {
+            led.toggle(X2, X1 + A)
+            basic.pause(100)
+        }
+        for (let A = 0; A <= B; A++) {
+            led.toggle(X3 - A, X2)
+            basic.pause(100)
+        }
+        B = B - 1
+        for (let A = 0; A <= B; A++) {
+            led.toggle(X, X3 - A)
+            basic.pause(100)
+        }
     }
 }
 basic.forever(function () {
